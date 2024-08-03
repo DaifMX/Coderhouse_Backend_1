3afab0d1b02072120981bde561225ebf8419efe3
DaifMX <applediegoaif@gmail.com>
2024-08-03 10:08:25 -0600

    C

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import {Server} from 'socket.io'
+import "dotenv/config"
 import mongoose from 'mongoose';
 import ProductRouter from './routers/ProductRouter.js';
 import CartRouter from './routers/CartRouter.js';
 import ViewRouter from './routers/ViewRouter.js';
 import {__dirname} from './utils.js';
 
+// -------------------------------------------------------------------------------------------------------
 const app = express();
 const PORT = process.env.PORT || 8080;
-const CONNECTION_STRING = 'mongodb+srv://daif:alex1172@cluster.o7vhcun.mongodb.net/CoderEats?retryWrites=true&w=majority&appName=Cluster';
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
 
 mongoose.connect(CONNECTION_STRING);
 
